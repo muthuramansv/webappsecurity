1f57035a08b18d7da24797709cfa08ffcba5a2f6
Joshua Becker <joshua.becker@student.fh-kiel.de>
2018-05-03 17:53:07 +0200

    Session Mangement added

--- Session/index.php
+++ Session/index.php
@@ -1,9 +1,10 @@
 <?php
  include 'session.php';
- $mysession = new CostumSession;
+ $mysession = new CostumSession('WAS-Secure-Shop', 1800, '/', '127.0.0.1', false, true);
  print($mysession->getToken()); 
  print('<br>');
  print($_SESSION['token']);
  print('<br>');
- print($mysession->old_token);
-?>
+ print(session_id());
+ print($mysession->validateToken('123'));
+ ?>
--- Session/session.php
+++ Session/session.php
@@ -1,37 +1,44 @@
 <?php 
 class CostumSession {
     private $token = null;  
-    public $old_token = null; //just for debugging
-    function __construct() {
-        session_start();
-        $this->generateToken();
+    function __construct($name, $lifetime, $path, $domain, $secure, $httponly) {
+        session_start([
+            'name' => $name,
+            'cookie_lifetime' => $lifetime,
+            'cookie_httponly' => $httponly,
+            'use_strict_mode' => true,
+            'cookie_domain' => '127.0.0.1',
+        ]);
+        session_create_id($this->generator());
+        if(!(isset($_SESSION['token']))){
+            $this->generateToken();   
+        }
+        $this->token = $_SESSION['token'];
     }
-    function saveInSession($key, $value) {
+
+    private function generator(){
+        return md5(openssl_random_pseudo_bytes(32));
+    }
+
+    public function saveInSession($key, $value) {
         if($key != 'token'){
             $_SESSION[$key] = $value;
         }
-    } 
-    private function checkForOldToken(){
-        if($this->old_token){
-            return true;
-        }
-        return false;
     }
+    
     private function generateToken() {
-        if(isset($_SESSION['token'])){
-            $this->old_token = $_SESSION['token'];
-        } 
-        $this->token = md5(openssl_random_pseudo_bytes(32));
+        $this->token = $this->generator();
         $_SESSION['token'] = $this->token;
     }
+    
     public function getToken(){
         return $this->token;
     }
-    function validateToken($form_token){
-        if($this->checkForOldToken){
-            if($form_token == $this->old_token){ //Case: $form_token = "null"...
-                return true;
-            }
+    
+    public function validateToken($form_token){
+        if($form_token === $this->getToken()){ 
+            $this->generateToken();
+            return true;
         }
         return false;
     }
