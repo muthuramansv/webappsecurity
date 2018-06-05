4d69c39ccb587f279a8fdca20edf52eaea59583e
muthuramansv <muthu_ramansv@hotmail.com>
2018-06-05 23:24:24 +0200

    Update signuphandler.php

--- Integration/classes/signuphandler.php
+++ Integration/classes/signuphandler.php
@@ -19,13 +19,13 @@ $connectToDb = new SimpleConnectDB();
 $arrayTest = $connectToDb->get_tbl_items();
 
 if(isset($POST['register'])){
-    $username = mysqli_real_escape_string($_POST['username']);
-    $firstname = mysqli_real_escape_string($_POST['firstname']);
-    $lastname = mysqli_real_escape_string($_POST['lastname']);
-    $email = mysqli_real_escape_string($_POST['email']);
-    $password_1 = mysqli_real_escape_string($_POST['password_1']);
-    $password_2 = mysqli_real_escape_string($_POST['password_2']);
-    $address = mysqli_real_escape_string($_POST['address']);
+    $username = isset($_POST['username']);
+    $firstname = isset($_POST['firstname']);
+    $lastname = isset($_POST['lastname']);
+    $mail = isset($_POST['email']);
+    $password_1 = isset($_POST['password_1']);
+    $password_2 = isset($_POST['password_2']);
+    $address = isset($_POST['address']);
 
     if(empty($username)){
         $array_push($errors, "Username is required");
@@ -55,11 +55,9 @@ if(isset($POST['register'])){
         $array_push($errors,"Passwords do not match");
         }
 
-        if(count($errors)== 0){
         $password = md5($password_1);
-        $sql = "INSERt INTO users (username, email, password, address,fisrtname, lastname)
-                              VALUES ('$username', '$email', '$password_1','$address','$firstname','$lastname')";
-        mysqli_query($db, $sql);
+    $connectToDb = new SimpleConnectDB();
+    $connectToDb->set_tbl_user($firstname, $lastname, $address, $email, $password);
         }
     }
 ?>
