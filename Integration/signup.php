26d965f657cdc7e9741a697af8ff03a251c3cb57
STUDIUM\Damian <damian-ulbrich@gmx.net>
2018-06-07 17:21:57 +0200

    Commit

--- Integration/signup.php
+++ Integration/signup.php
@@ -1,6 +1,6 @@
 <?php
 include 'classes/pageBuilder.php';
-include 'classes/connect_class1.php';
+include 'classes/connect_class.php';
 include 'classes/signuphandler.php';
 
 
@@ -11,7 +11,7 @@ $html_code  = "<html>
         <h2>Sign Up</h2>
         <p>Please fill this form to create an account.</p>
         <form method='POST' action=\"classes\signuphandler.php\">
-       
+
 		    <div class=\"input-group\">
                 <label>First Name</label>
                 <input type=\"text\" name=\"firstname\"class=\"form-control\" placeholder='Enter First Name' >
@@ -23,29 +23,29 @@ $html_code  = "<html>
             <div class=\"input-group\">
                 <label>Username</label>
                 <input type=\"text\" name=\"username\" placeholder='Enter Username' >
-            </div> 
+            </div>
              <div class=\"input-group\">
                 <label>E-Mail ID</label>
-                <input type=\"text\" name=\"email\"class=\"form-control\" placeholder='Enter Valid Email Id' >                
-             </div>			
+                <input type=\"text\" name=\"email\"class=\"form-control\" placeholder='Enter Valid Email Id' >
+             </div>
             <div class=\"input-group\">
                 <label>Password</label>
                 <input type=\"password\" name=\"password_1\" class=\"form-control\" placeholder='Enter Password' >
             </div>
             <div class=\"input-group\">
                 <label>Confirm Password</label>
-                <input type=\"password\" name=\"password_2\" class=\"form-control\" placeholder='Re-Enter Password' >                
+                <input type=\"password\" name=\"password_2\" class=\"form-control\" placeholder='Re-Enter Password' >
             </div>
             <div class=\"input-group\">
             <label>Address</label>
-            <textarea type=\"text\" name=\"address\" rows=\"4\" cols=\"50\"  class=\"input - group\" >                  
+            <textarea type=\"text\" name=\"address\" rows=\"4\" cols=\"50\"  class=\"input - group\" >
             </textarea>
             <div class=\"input-group\">
             <button input type=\"submit\" class=\"btn btn-primary\" value=\"submit\"> Submit</button>
             </div>
             <p>Already have an account? <a href=\"login.php\">Login here</a>.</p>
         </form>
-    </div>    
+    </div>
 </body>
 </html>";
 
