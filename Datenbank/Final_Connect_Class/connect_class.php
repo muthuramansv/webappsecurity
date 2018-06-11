2b8233134c93ee2a2c2be9b06c41d546a52006cf
STUDIUM\Damian <damian-ulbrich@gmx.net>
2018-06-11 10:05:02 +0200

    Copied Connect_class into Datenbanken too.

--- Datenbank/Final_Connect_Class/connect_class.php
+++ Datenbank/Final_Connect_Class/connect_class.php
@@ -104,6 +104,33 @@ class SimpleConnectDB
 
 		}
 
+		public function get_tbl_user_login_with_token()  {
+
+				$con = $this->connect();
+
+				if($stmt = $con->prepare("SELECT firstname, pass, mail, token  FROM tbl_user "))
+				{
+
+				$stmt->execute();
+
+				$res = $stmt->get_result();
+
+				$array = $res->fetch_all();
+
+				return $array;
+
+				}else {
+
+				$error = $con->errno . ' ' . $con->error;
+				    echo $error;
+
+				}
+
+				$stmt->close();
+				$con->close();
+
+		}
+
     /**
      * @return mixed
      */
@@ -226,7 +253,48 @@ class SimpleConnectDB
      * @param $mail_
      * @param $pass_
      */
-    public function set_tbl_user($firstname_, $lastname_, $address_, $mail_, $pass_) {
+
+	
+    public function set_tbl_user($firstname_, $lastname_, $address_, $mail_, $pass_, $token_) {
+
+	$con = $this->connect();
+	$query = "INSERT INTO tbl_user (`firstname`, `lastname`, `address`, `mail`, `pass`, `token`) VALUES ((?), (?), (?), (?), (?), (?));";
+
+	if ($stmt = $con->prepare($query)) {
+
+		if (!$stmt->bind_param("sssss", $firstname, $lastname, $address, $mail, $pass, $token)) {
+			echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+		}
+
+		$firstname = $firstname_;
+		$lastname = $lastname_;
+		$address = $address_;
+		$mail = $mail_;
+		$pass = password_hash($pass_, PASSWORD_DEFAULT, ['cost' => 11]);
+		$token = $token_;
+
+
+
+		echo $firstname_ . "<br>" . $lastname_ . "<br>" . $address_ . "<br>" . $mail_ .  "<br>" . $pass_ . "<br>" .  "Done" . "<br>";
+
+		if(!($query_result=$stmt->execute())) {
+			echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+			echo "execute"."<br>";
+		}
+
+		echo $firstname . "<br>" . $lastname . "<br>" . $address . "<br>" . $mail .  "<br>" . $pass . "<br>" .  "Done" . "<br>";
+
+	} else {
+		$error = $con->errno . ' ' . $con->error;
+		echo "else";
+		echo $error;
+	}
+
+	$stmt->close();
+	$con->close();
+}
+
+public function set_tbl_user_without_token($firstname_, $lastname_, $address_, $mail_, $pass_) {
 
 	$con = $this->connect();
 	$query = "INSERT INTO tbl_user (`firstname`, `lastname`, `address`, `mail`, `pass`) VALUES ((?), (?), (?), (?), (?));";
@@ -241,7 +309,8 @@ class SimpleConnectDB
 		$lastname = $lastname_;
 		$address = $address_;
 		$mail = $mail_;
-		$pass = $pass_;
+		$pass = password_hash($pass_, PASSWORD_DEFAULT, ['cost' => 11]);
+		
 
 
 
@@ -266,11 +335,64 @@ class SimpleConnectDB
 
 // Check User 1.1
 
-public function checkUSER($checkmail)  {
+
+public function checkUSER($checkmail, $checkpassword)  {
+
+
+
 
 		$con = $this->connect();
 
-		if($stmt = $con->prepare("SELECT `mail` FROM tbl_user WHERE `mail` = '$checkmail' "))
+		$checkmail_ = mysqli_real_escape_string($con, $checkmail);
+		$checkpassword_ = mysqli_real_escape_string($con, $checkpassword);
+
+		if($stmt = $con->prepare("SELECT `mail` , `password` FROM tbl_user WHERE `mail` = '$checkmail_' and `password` = '$checkpassword_' "))
+		{
+
+		$stmt->execute();
+
+		$res = $stmt->get_result();
+
+		$array = $res->fetch_all();
+
+		if($array) {
+
+			echo "User already exists !";
+
+			return 1;
+		}
+
+		else {
+
+			echo "Usermail free";
+			return 0;
+		}
+
+
+		}else {
+
+		$error = $con->errno . ' ' . $con->error;
+		    echo $error;
+
+		}
+
+		$stmt->close();
+		$con->close();
+
+		}
+		
+		public function checkUSER_All($checkmail, $checkpassword)  {
+
+
+
+
+		$con = $this->connect();
+
+		$checkmail_ = mysqli_real_escape_string($con, $checkmail);
+		$checkpassword_ = mysqli_real_escape_string($con, $checkpassword);
+		
+
+		if($stmt = $con->prepare("SELECT * FROM tbl_user WHERE `mail` = '$checkmail_' AND `pass` = '$checkpassword_' "))
 		{
 
 		$stmt->execute();
@@ -306,8 +428,48 @@ public function checkUSER($checkmail)  {
 		}
 
 
-// Insert USer 1.2
 
+
+		public function GetUserToken($checktoken)  {
+
+		$con = $this->connect();
+
+		$checktoken_ = mysqli_real_escape_string($con, $checktoken);
+
+		if($stmt = $con->prepare("SELECT `mail` FROM tbl_user WHERE `token` = '$checktoken_' "))
+		{
+
+		$stmt->execute();
+
+		$res = $stmt->get_result();
+
+		$array = $res->fetch_all();
+
+		if($array) {
+
+			echo "Token already exists !";
+
+			return 1;
+		}
+
+		else {
+
+			echo "Token does not exists";
+			return 0;
+		}
+
+
+		}else {
+
+		$error = $con->errno . ' ' . $con->error;
+		    echo $error;
+
+		}
+
+		$stmt->close();
+		$con->close();
+
+		}
 
     /**
      * @param $id_user_
@@ -353,8 +515,6 @@ public function checkUSER($checkmail)  {
 	$stmt->close();
 	$con->close();
 }
-
-
 
 }
 
