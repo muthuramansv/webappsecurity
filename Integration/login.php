8e34f01d15a4a7c925da5ee6381e487dec8a8f1f
muthuramansv <muthu_ramansv@hotmail.com>
2018-06-12 22:59:48 +0200

    Update login.php

--- Integration/login.php
+++ Integration/login.php
@@ -1,5 +1,8 @@
 <?php
 include 'classes/pageBuilder.php';
+include 'classes/log_in_validation.php';
+
+LoginValidation::login_management();
 
 //Never used Bootstrap CSS classes? //Also Interactions should be secured using CSRF
 $html_code  = "<html>"
